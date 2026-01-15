1d93252bf5e04230296dee8d5628fc0fcb1f4fd0
MranasSir <eloualianas07@gmail.com>
2026-01-15 15:11:57 +0100

    Week 6 Semi2.0 Tamagochi

--- Project Tamagochi/script.js
+++ Project Tamagochi/script.js
@@ -87,3 +87,90 @@ function sleepPet() {
 
 // Update immediately on load
 updateStats();
+//===========================//
+//  PIXEL ART RENDERING   // 
+//===========================//
+// ===== CANVAS SETUP =====
+const canvas = document.getElementById('game'); // make sure your HTML has <canvas id="game" width="64" height="64"></canvas>
+const ctx = canvas.getContext('2d');
+ctx.imageSmoothingEnabled = false; // keep pixels sharp when scaled
+
+// clear canvas
+function clearScreen() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+// draw a single pixel
+function drawPixel(x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, 1, 1);
+}
+
+// ===== BIG PENGUIN SPRITE =====
+function drawBigPenguin() {
+  const xOffset = 4;
+  const yOffset = 30;
+  // Body/outline (black)
+  const blackPixels = [
+    // Head top
+    [24,5],[25,5],[26,5],[27,5],[28,5],[29,5],[30,5],[31,5],
+    [23,6],[24,6],[25,6],[26,6],[27,6],[28,6],[29,6],[30,6],[31,6],[32,6],
+    [23,7],[24,7],[25,7],[26,7],[27,7],[28,7],[29,7],[30,7],[31,7],[32,7],
+    [22,8],[23,8],[24,8],[25,8],[26,8],[27,8],[28,8],[29,8],[30,8],[31,8],[32,8],[33,8],
+    // Body
+    [21,9],[22,9],[23,9],[24,9],[25,9],[26,9],[27,9],[28,9],[29,9],[30,9],[31,9],[32,9],[33,9],[34,9],
+    [21,10],[22,10],[23,10],[24,10],[25,10],[26,10],[27,10],[28,10],[29,10],[30,10],[31,10],[32,10],[33,10],[34,10],
+    [20,11],[21,11],[22,11],[23,11],[24,11],[25,11],[26,11],[27,11],[28,11],[29,11],[30,11],[31,11],[32,11],[33,11],[34,11],[35,11],
+    [20,12],[21,12],[22,12],[23,12],[24,12],[25,12],[26,12],[27,12],[28,12],[29,12],[30,12],[31,12],[32,12],[33,12],[34,12],[35,12],
+    [19,13],[20,13],[21,13],[22,13],[23,13],[24,13],[25,13],[26,13],[27,13],[28,13],[29,13],[30,13],[31,13],[32,13],[33,13],[34,13],[35,13],[36,13],
+    [19,14],[20,14],[21,14],[22,14],[23,14],[24,14],[25,14],[26,14],[27,14],[28,14],[29,14],[30,14],[31,14],[32,14],[33,14],[34,14],[35,14],[36,14],
+    // Wings
+    [18,15],[19,15],[36,15],[37,15],
+    [18,16],[19,16],[36,16],[37,16],
+    [17,17],[18,17],[37,17],[38,17],
+    [17,18],[18,18],[37,18],[38,18],
+    [17,19],[18,19],[37,19],[38,19]
+  ];
+  blackPixels.forEach(p => drawPixel(p[0] + xOffset, p[1] + yOffset, "#000"));
+
+  // Belly (white)
+  const whitePixels = [
+    [25,9],[26,9],[27,9],[28,9],[29,9],[30,9],
+    [25,10],[26,10],[27,10],[28,10],[29,10],[30,10],
+    [26,11],[27,11],[28,11],[29,11],
+    [26,12],[27,12],[28,12],[29,12],
+    [26,13],[27,13],[28,13],[29,13]
+  ];
+  whitePixels.forEach(p => drawPixel(p[0] + xOffset, p[1] + yOffset, "#fff"));
+
+  // Eyes (white)
+  drawPixel(26 + xOffset,7 + yOffset,"#fff");
+  drawPixel(29 + xOffset,7 + yOffset,"#fff");
+
+  // Pupils (black)
+  drawPixel(26 + xOffset,7 + yOffset,"#000");
+  drawPixel(29 + xOffset,7 + yOffset,"#000");
+
+  // Beak (orange)
+  drawPixel(27 + xOffset,10 + yOffset,"#FFA500");
+  drawPixel(28 + xOffset,10 + yOffset,"#FFA500");
+
+  // Feet (orange)
+  drawPixel(24 + xOffset,15 + yOffset,"#FFA500");
+  drawPixel(25 + xOffset,15 + yOffset,"#FFA500");
+  drawPixel(30 + xOffset,15 + yOffset,"#FFA500");
+  drawPixel(31 + xOffset,15 + yOffset,"#FFA500");
+}
+
+// ===== GAME LOOP =====
+function gameLoop() {
+  clearScreen();
+  drawBigPenguin();
+}
+
+// run 10 FPS
+setInterval(gameLoop, 100);
+
+
+
+
